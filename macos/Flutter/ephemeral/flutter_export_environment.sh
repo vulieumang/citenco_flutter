30fc830c1661c4d92fe123527d80ad6c1c4ccf76
Nguyen <johndoe@email.com>
2022-10-16 21:44:30 +0700

    release

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/vu/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/vu/Documents/GitHub/citenco_flutter"
+export "FLUTTER_ROOT=C:\Users\Hieu Ngo\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Hieu Ngo\Downloads\citenco_flutter-main\citenco_flutter-main\citenco_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
